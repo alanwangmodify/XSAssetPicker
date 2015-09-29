d8b12d7daf7247214907cf24abfd05be80630af2
xiaoshu.wang@choumei.cn <alanwangmodify@126.com>
2015-09-29 10:05:01 +0800

    Initial Commit

---
+++ XSAssetPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XSAssetPicker
+//
+//  Created by choumei on 15/9/29.
+//  Copyright (c) 2015年 WangXiaoShu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
